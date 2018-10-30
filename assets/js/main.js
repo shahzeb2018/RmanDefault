0e4511f1b8738c7ee8000ddb7e07fc7aceab8b3f
shahzeb2018 <42054267+shahzeb2018@users.noreply.github.com>
2018-10-30 11:35:04 -0400

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,12 +22,6 @@
 			xsmall:   [ null,      '480px'  ],
 		});
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
 
 	// Tweaks/fixes.
 
